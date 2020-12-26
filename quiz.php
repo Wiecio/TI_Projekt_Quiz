8485cb911d1751850073274106cbfa09819a8605
Wiecio <62389416+Wiecio@users.noreply.github.com>
2020-12-26 15:46:41 +0100

    Answers as radio buttons

--- quiz.php
+++ quiz.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
-$_SESSION['corrAns'] = 'cardB';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Quizzzzzz|learn</title>
+<title>Quizzzzzz | Learn</title>
 </head>
 <body>
 <?php include 'header.php';?>
@@ -14,43 +13,18 @@ $_SESSION['corrAns'] = 'cardB';
 	
 <script>
 
-var answersActive = true;
-
-function nextClicked(textA,textB,textC,textD,textQuestion,idCorrect){
-  document.getElementById("A").innerHTML = textA;
-  document.getElementById("B").innerHTML = textB;
-  document.getElementById("C").innerHTML = textC;
-  document.getElementById("D").innerHTML = textD;
-  document.getElementById("question").innerHTML = textQuestion;
-  
-  const cardList = document.querySelectorAll('.card');
-	
-  cardList.forEach(element => {
-	element.classList.replace('bg-danger','bg-secondary');
-  });
-	
-  document.getElementById(idCorrect).classList.replace('bg-success','bg-secondary');
-  document.getElementById('nextButton').setAttribute('disabled','true');
-  answersActive = true;
-	
-}
-
-function answerClicked (idCorrect){
-	if(answersActive){
-		
-		const cardList = document.querySelectorAll('.card');
-		
-		cardList.forEach(element => {
-			element.classList.replace('bg-secondary','bg-danger');
+function answerClicked (idSelected){
 			
-		});
-
-		document.getElementById(idCorrect).classList.replace('bg-danger','bg-success');
-		document.getElementById('nextButton').removeAttribute('disabled');
-		
-		answersActive = false;
-	}
+	const cardList = document.querySelectorAll('.card');
 	
+	cardList.forEach(element => {
+		element.classList.replace('bg-success','bg-secondary');
+		
+	});
+	
+	document.getElementById(idSelected).classList.replace('bg-secondary','bg-success');
+	document.getElementById('nextButton').removeAttribute('disabled');
+			
 }
 
 </script>	
@@ -59,64 +33,38 @@ function answerClicked (idCorrect){
 
 	
 <div class="container mt-5">
-	
-	<div class="card">
-		<div class="row mt-5">
-			<div class="col-10 card-title text-center mx-auto mt-2 text-primary">
-				<h2 id="question">To jest przykładowe pytanie, które normalnie byłoby wczytane przez php?</h2>		
-			</div>
-		</div>	
-		
-		<div class="card-body">
-			<div class="row row-cols-2 mt-3 mx-auto col-md-8">
-				
-				<a class="btn btn-fix text-left" onClick="answerClicked('<?= $_SESSION['corrAns']?>')">		
-					<div id='cardA' class="card text-white bg-secondary mb-3 float-center"  >
-						<div class="card-body">
-							<h5 class="card-title">A.</h5>
-							<p class="card-text" id="A">Treść odpowiedzi wczytana przez php</p>
-						</div>
-					</div>
-				</a>
-			
-			
-			
-				<a class="btn btn-fix text-left" onClick="answerClicked('<?= $_SESSION['corrAns']?>')">		
-					<div id='cardB' class="card text-white	bg-secondary mb-3">
-						<div class="card-body">
-							<h5 class="card-title">B.</h5>
-							<p class="card-text" id="B">Treść odpowiedzi wczytana przez php</p>
-						</div>
-					</div>
-				</a>
-			
-			
-				<a class="btn btn-fix text-left" onClick="answerClicked('<?= $_SESSION['corrAns']?>')">		
-					<div  id='cardC' class="card text-white bg-secondary mb-3">
-						<div class="card-body">
-							<h5 class="card-title">C.</h5>
-							<p class="card-text" id="C">Treść odpowiedzi wczytana przez php</p>
-						</div>
-					</div>
-				</a>
-
-				<a class="btn btn-fix text-left" onClick="answerClicked('<?= $_SESSION['corrAns']?>')">		
-					<div id='cardD' class="card text-white bg-secondary mb-3" >
-						<div class="card-body">
-							<h5 class="card-title">D.</h5>
-							<p class="card-text" id="D">Treść odpowiedzi wczytana przez php</p>
-						</div>
-					</div>
-				</a>
-				
-				<button type="button" id="nextButton" class="btn btn-lg btn-primary col-6 mx-auto mt-2 mb-3" onClick="nextClicked('Nowe A','Nowe B','Nowe C','Nowe D','Nowe pytanie wczytane przez php?','<?= $_SESSION['corrAns']?>')" disabled>
-					Next </button>
-				
+	<form>
+		<div class="card">
+			<div class="row mt-5">
+				<div class="col-10 card-title text-center mx-auto mt-2 text-primary">
+					<h2 id="question">To jest przykładowe pytanie, które normalnie byłoby wczytane przez php?</h2>		
+				</div>
 			</div>	
-				
-		
-		</div>	
-	</div>
+			
+			<div class="card-body">
+				<div class="row row-cols-2 mt-3 mx-auto col-md-8">
+					
+				<!--to trzeba zapetlic w php-->	
+					<input type="radio" name="answer" value="A" id="A" style="display: none;">
+					<label for="A">
+						<a class="btn btn-fix text-left" onClick="answerClicked('cardA')">	
+							<div id='cardA' class="card text-white bg-secondary mb-3 float-center"  >
+								<div class="card-body">
+									<h5 class="card-title">A.</h5>
+									<p class="card-text">Treść odpowiedzi wczytana przez php</p>
+								</div>
+							</div>
+						</a>
+					</label>
+				<!--------------------------->	
+
+
+				</div>	
+			</div>	
+			<button type="submit" id="nextButton" class="btn btn-lg btn-primary col-6 mx-auto mt-2 mb-3" disabled>
+						Next </button>
+		</div>
+	</form>
 </div>
 
 	
