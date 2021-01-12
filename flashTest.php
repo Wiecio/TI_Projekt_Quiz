e41242758ca9ac49295637b0efca5c7e280ea15b
Wiecio <62389416+Wiecio@users.noreply.github.com>
2021-01-12 13:46:45 +0100

    Merge branch 'master' into Wiecio_branch

--- flashTest.php
+++ flashTest.php
@@ -1,3 +1,73 @@
+<?php
+session_start();
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+			if(isset($_POST['check']))
+			{
+					$_SESSION['check'] = true;
+					$back = mb_strtolower($_POST['back'],'UTF-8');
+					$back_corr = mb_strtolower($_SESSION['tab_flash'][$_SESSION['counter']][1],'UTF-8');
+					if($back == $back_corr)
+					{
+						$_SESSION['comm'] = "Good!";
+					}
+					else
+					{
+						$_SESSION['comm'] = "Wrong! :("." Your answer: ".$back."  "."Correct answer: ".$back_corr;
+					}
+			}
+			else if(isset($_POST['next']))
+			{
+				if($_SESSION['counter']==	$_SESSION['max'])
+				{
+					$_SESSION['comm'] = "You are on the last flashcard!";
+				}
+				else
+				{
+					$_SESSION['counter']++;
+					$_SESSION['check'] = false;
+				}
+			}
+			else if(isset($_POST['jump']))
+			{
+				$_SESSION['counter']=0;
+			}
+			else
+			{
+					require_once "db_connect.php";
+					$tab_name = key($_POST);
+					try
+					{
+							$_SESSION['countf'] = 0;
+							$conn  = new mysqli($host,$db_user,$db_password,$db_name);
+							if($conn->connect_errno!=0)
+							{
+									throw new Exception(mysqli_connect_errno());
+							}
+							$_SESSION['tab_flash'] = array();
+							$sql = "SELECT * FROM $tab_name";
+							$r = $conn->query($sql);
+							$i=0;
+							while($w = $r->fetch_assoc())
+							{
+									$_SESSION['tab_flash'][$i][0] = $w['first_p'];
+									$_SESSION['tab_flash'][$i][1] = $w['second_p']; 
+									$i++;
+							}
+							$_SESSION['max'] = $i-1;
+							$conn->close();
+							$_SESSION['check'] = false;
+							$_SESSION['counter']=0;
+									
+									
+					}
+					catch(Exception $e)
+					{
+	
+					}
+			}
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -51,29 +121,60 @@
 <div class="container mt-5" >
 	<div class="row">
 		<div class="mx-auto mt-5 col-12 col-md-8 col-lg-6">
-		<div class="alert alert-danger mt-3">Komunikat</div>
+	<?php if($_SESSION['check']) : ?>
+		<?php if(isset($_SESSION['comm']) && $_SESSION['comm'] == "Good!" ) : ?>
+			<div class="alert alert-success mt-3"><?=$_SESSION['comm']?></div>
+				<?php if(isset($_SESSION['comm'])) unset($_SESSION['comm']) ?>
+		<?php elseif(isset($_SESSION['comm'])) :?>
+				<div class="alert alert-danger mt-3"><?=$_SESSION['comm']?></div>
+				<?php if(isset($_SESSION['comm'])) unset($_SESSION['comm']) ?>
+		<?php endif;?>
+	<?php endif ;?>
 			<form action="" method="POST">
 				<div class="mt-3">
 					<div class="flip-card mt-3">
 						<div id="flashCard" class="flip-card-inner">
 							<div class="flip-card-front">
-								<h1 style="margin-top:65px;">Front</h1>		
+								<h1 style="margin-top:65px;"><?=$_SESSION['tab_flash'][$_SESSION['counter']][0]?></h1>		
 							</div>
 						
 						</div>
 					</div>	
-					1/10
-					<div class="col-12 mt-2">
-						<input type="text" id="back" name="" placeholder="Back" required class="form-control" />
-					</div>
+					<?=	$_SESSION['counter']+1?>/<?=$_SESSION['max']+1?>
+						<?php if($_SESSION['check'] == false): ?>
+								<div class="col-12 mt-2">
+									<input type="text" id="back" name="back" placeholder="Back" required class="form-control" />
+								</div>		
+						<?php else :?>
+								<div class="col-12 mt-2">
+									<input type="text" id="back" name="back" placeholder="Back" class="form-control" />
+								</div>
+						<?php endif; ?>	
+				
 					
 					<div class="row mt-4"> 				
 						<div class="col-6">
-							<button type="submit" class="btn btn-lg btn-block btn-success float-left" name="" >Check</button>
+							<button type="submit" class="btn btn-lg btn-block btn-success float-left" name="check" >Check</button>
 						</div>
-						<div class="col-6">
-							<button type="submit" class="btn btn-lg btn-block btn-success float-right" name="">Next</button>
-						</div>								
+						<?php if($_SESSION['check'] == false): ?>
+							<div class="col-6">
+								<button disabled type="submit" class="btn btn-lg btn-block btn-success float-right" name="next">Next</button>
+							</div>		
+						<?php else :?>
+							<div class="col-6">
+								<button type="submit" class="btn btn-lg btn-block btn-success float-right" name="next">Next</button>
+							</div>
+						<?php endif; ?>	
+						<?php if($_SESSION['check'] == false): ?>
+							<div class="col-12">
+								<button disabled type="submit" class="btn btn-lg btn-block btn-success float-right mt-2" name="jump">Jump to the begining</button>
+							</div>		
+						<?php else :?>
+							<div class="col-12">
+									<button type="submit" class="btn btn-lg btn-block btn-success float-right mt-2" name="jump">Jump to the begining</button>
+							</div>	
+						<?php endif; ?>			
+								
 					</div>
 				</div>	
 			</form>
