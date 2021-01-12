9a68790b90002551eb2c4aaae22a49465b175a9d
Wiecio <62389416+Wiecio@users.noreply.github.com>
2021-01-12 13:06:23 +0100

    Fixed alignment

--- quizView.php
+++ quizView.php
@@ -62,52 +62,43 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	
 <div class="container mt-5" >
 <form action="saveStatQuiz.php" method="post">
-	<div class="row">
-		<div class="col-12">
-			
-			<div class="text-center">
-				<h1 class="text-dark ">View your quiz</h1>	
-				<div class="mb-3 mx-auto custom-control custom-checkbox">
-					<input type="checkbox" class="custom-control-input" id="is_public" name="is_public" checked>
-					<label class="custom-control-label mb-3" for="is_public">This quiz is public</label>
-				</div>
-			</div>
-			<div class="card mt-2 mb-5">								
-			<div class="card-body">				
-			<div class="mt-2 mb-5 ">								
-				
-					<div class="row">		
-						<div class="mx-auto col-12 col-md-8">			
-							<ul class="list-group list-group-flush ">
-								<?php $counter = 0; ?>
-								<?php while($w = $r->fetch_assoc()) :?>
-									<h3 class="list-group-item text-dark text-left mt-3"><?=++$counter?>. <?=$w['question']?></h3>
-									<?php 
-										$ans_tab = explode(",",$w['answers']);
-
-									?>
-									<?php for($i=1;$i<count($ans_tab);$i++) :?>
-									
-											<?php if( mb_substr($ans_tab[$i], mb_strlen($ans_tab[$i])-1, mb_strlen($ans_tab[$i]), 'UTF-8') == ":") :?>
-											<?php $ans_tab[$i] = mb_substr($ans_tab[$i], 0, mb_strlen($ans_tab[$i])-1, 'UTF-8') ?>
-												<h4 class='text-success ml-5'><?=$tab[$i]?>. <?=$ans_tab[$i]?></h4>
-											<?php else :?>
-												<h4 class="text-secondary ml-5"><?=$tab[$i]?>. <?=$ans_tab[$i]?></h4>
-											<?php endif;?>
-									<?php endfor ;?>
-								<?php endwhile ;?>
-							 
-							</ul>							
-							
-						</div>
-																																			
-					</div>							
-					
-			</div>	
-			</div>
-			</div>
-		</div>		
+	
+	<div class="text-center">
+		<h1 class="text-dark ">View your quiz</h1>	
+		<div class="mb-3 mx-auto custom-control custom-checkbox">
+			<input type="checkbox" class="custom-control-input" id="is_public" name="is_public" checked>
+			<label class="custom-control-label" for="is_public">This quiz is public</label>
+		</div>
 	</div>
+	<div class="card mt-2 mb-5">								
+		<div class="card-body">															
+			<div class="row col-12 col-md-8 mx-auto">		
+				<div class="mx-auto">	
+					<ul class="list-group list-group-flush ">
+						<?php $counter = 0; ?>
+						<?php while($w = $r->fetch_assoc()) :?>
+							<h3 class="list-group-item text-dark mt-3"><?=++$counter?>. <?=$w['question']?></h3>
+							<?php 
+								$ans_tab = explode(",",$w['answers']);
+
+							?>
+							<?php for($i=1;$i<count($ans_tab);$i++) :?>
+							
+									<?php if( mb_substr($ans_tab[$i], mb_strlen($ans_tab[$i])-1, mb_strlen($ans_tab[$i]), 'UTF-8') == ":") :?>
+									<?php $ans_tab[$i] = mb_substr($ans_tab[$i], 0, mb_strlen($ans_tab[$i])-1, 'UTF-8') ?>
+										<h4 class='text-success ml-5'><?=$tab[$i]?>. <?=$ans_tab[$i]?></h4>
+									<?php else :?>
+										<h4 class="text-secondary ml-5"><?=$tab[$i]?>. <?=$ans_tab[$i]?></h4>
+									<?php endif;?>
+							<?php endfor ;?>
+						<?php endwhile ;?>
+					 
+					</ul>	
+				</div>																																					
+			</div>										
+		</div>
+	</div>
+
 	<div class="row mb-5 mx-auto col-12">
 		<button type="submit" class="btn btn-lg btn-primary col-3 mx-auto" name="save_stat">Save</button>
 	</div>
