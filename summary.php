0314e72503fd15cf3ad8b22b76eb08a222bce322
PianistaPiano <76052736+PianistaPiano@users.noreply.github.com>
2021-01-19 13:18:00 +0100

    update summary

--- summary.php
+++ summary.php
@@ -5,30 +5,60 @@ if(isset($_SESSION['load']) && !isset($_SESSION['tab_name']))
 {
 	if(isset($_SESSION['log_in']))
 	{
-		try
+		if(substr($_SESSION['quiz'],0,1) == "s")
 		{
-			$conn  = new mysqli($host,$db_user,$db_password,$db_name);
-			if($conn->connect_errno!=0)
+			try
 			{
-				throw new Exception(mysqli_connect_errno());
+				$conn  = new mysqli($host,$db_user,$db_password,$db_name);
+				if($conn->connect_errno!=0)
+				{
+					throw new Exception(mysqli_connect_errno());
+				}
+				$tab_name = "namequiz_".$_SESSION['user_id'];
+				$id_quiz = $_SESSION['id_quiz'];
+				$sql = "SELECT name_quiz FROM $tab_name WHERE id_quiz = $id_quiz";
+				$r = $conn->query($sql);
+				$w = $r->fetch_assoc();
+				$nameQuiz = $w['name_quiz'];
+				$tab_name = "quiz".$id_quiz."_".$_SESSION['user_id'];
+				$sql = "SELECT * FROM $tab_name";
+				$r = $conn->query($sql);
+				$tab = array(" ","A","B","C","D");
+				$j=0;
 			}
-			$ex_tab = explode("_",$_SESSION['quiz']);
-			$tab_name = "namequiz_".$ex_tab[1];
-			$id_quiz = $_SESSION['id_quiz'];
-			$sql = "SELECT name_quiz FROM $tab_name WHERE id_quiz = $id_quiz";
-			$r = $conn->query($sql);
-			$w = $r->fetch_assoc();
-			$nameQuiz = $w['name_quiz'];
-			$tab_name = $_SESSION['quiz'];
-			$sql = "SELECT * FROM $tab_name";
-			$r = $conn->query($sql);
-			$tab = array(" ","A","B","C","D");
-			$j=0;
+			catch(Exception $e)
+			{
+		
+			}
 		}
-		catch(Exception $e)
+		else
 		{
+			try
+			{
+				$conn  = new mysqli($host,$db_user,$db_password,$db_name);
+				if($conn->connect_errno!=0)
+				{
+					throw new Exception(mysqli_connect_errno());
+				}
+				$ex_tab = explode("_",$_SESSION['quiz']);
+				$tab_name = "namequiz_".$ex_tab[1];
+				$id_quiz = $_SESSION['id_quiz'];
+				$sql = "SELECT name_quiz FROM $tab_name WHERE id_quiz = $id_quiz";
+				$r = $conn->query($sql);
+				$w = $r->fetch_assoc();
+				$nameQuiz = $w['name_quiz'];
+				$tab_name = $_SESSION['quiz'];
+				$sql = "SELECT * FROM $tab_name";
+				$r = $conn->query($sql);
+				$tab = array(" ","A","B","C","D");
+				$j=0;
+			}
+			catch(Exception $e)
+			{
 
+			}
 		}
+		
 	}
 	else
 	{
